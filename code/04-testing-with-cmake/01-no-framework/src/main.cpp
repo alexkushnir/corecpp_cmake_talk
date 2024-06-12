999515932256d389734bc899ea50b7ff71d81a40
Alex Kushnir <kushnir.alexander@gmail.com>
2024-06-13 00:51:49 +0300

    Revert "Added an example of testing with CMake"
    This reverts commit bff0b82628f30205ee27d04727f67605ecd68b74.

--- code/04-testing-with-cmake/01-no-framework/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "RemoteLogMessage.h"
-#include <iostream>
-
-int main()
-{
-    messaging::RemoteLogMessage rlm{ 700, messaging::RemoteLogMessage::LogLevel::DebugLevel, "How to print a % number?", 7474 };
-}

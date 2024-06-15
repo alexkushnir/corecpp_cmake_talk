5799bf88140c7e34ac974ef87c2568af9ebf6761
Alex Kushnir <kushnir.alexander@gmail.com>
2024-06-15 22:18:34 +0300

    Reordered the lines in CMakeLists.txt and main.cpp

--- code/02-find_package/main.cpp
+++ code/02-find_package/main.cpp
@@ -14,11 +14,10 @@ void print_map(const MapType& m)
 int main()
 {
     boost::bimap<int, std::string> bm;
-    using bm_type = boost::bimap<int, std::string>;
-
-    bm.insert(bm_type::value_type(1, "one"));
-    bm.insert(bm_type::value_type(2, "two"));
-    bm.insert(bm_type::value_type(3, "three"));
+    
+    bm.insert(boost::bimap<int, std::string>::value_type(1, "one"));
+    bm.insert(boost::bimap<int, std::string>::value_type(2, "two"));
+    bm.insert(boost::bimap<int, std::string>::value_type(3, "three"));
 
     print_map(bm.left);
     std::cout << "\n";

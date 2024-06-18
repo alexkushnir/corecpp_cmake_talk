4fa78ac4aa9111f1ef373c2bb131a6fb8eb5ae7a
Alex Kushnir <kushnir.alexander@gmail.com>
2024-06-18 09:25:41 +0300

    Updated build script for Linux examples

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ Slides and code for Core C++ talk 19-Jun-2024
 * code/02-find-package uses the find_package() CMake feature to consume the boost C++ libraries
 * code/03-fetch-content demonstrates the usage of the FetchContent module by consuming the nlohmann::json package
 * code/04-cpp20-modules contains the basic usage of C++20 modules
+* code/build_examples_linux.sh - builds all examples on Linux, except 04-cpp20-modules
 
 ## Prerequisites:
-* All examples were tested on Ubuntu 22.04.3 with CMake 3.22 and g++ 11.4.0 except 03-cpp20-modules
+* All examples were tested on Ubuntu 22.04.3 with CMake 3.30.0-rc3 and g++ 11.4.0 except 03-cpp20-modules
 * Prior to running 02-find-package, run "sudo apt-get install libboost-dev"
 * 03-cpp20-modules was tested with CMake 3.29 and Visual Studio 2022 17.10 (Windows)
--- code/build_examples_linux.sh
+++ code/build_examples_linux.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
-# declare -A EXAMPLE_FOLDERS=("01-basic-example" "02-find_package" "03-fetch-content") # 04-cpp-modules is not for Linux 
 
-# printf "%s" "${EXAMPLE_FOLDERS[@]}"
-# for dir in "${EXAMPLE_FOLDERS[@]}"
-# do
-#    echo "$dir"
-#     echo "Configuring the $dir example"
-#     cmake -GNinja -S $dir -B $dir-build
-#     echo "Building the $dir example"
-#     cmake --build $dir-build
-# done
+if [ -z "$1" ]; then
+    echo "Usage: $0 <base-examples-directory>"
+    exit 1
+fi
 
-#!/bin/bash
+BASE_DIR=$1
 
-# Store directory names in an array
-directories=($(find . -maxdepth 1 -type d ! -name . | grep -v "modules"))
+directories=($(find $BASE_DIR -maxdepth 1 -mindepth 1 -type d | grep -v "modules"))
 
 # Iterate over the array and print each directory name
 for dir in "${directories[@]}"; 
 do
-
     # echo $dir
     echo "Configuring the $dir example"
     cmake -GNinja -S $dir -B $dir-build
